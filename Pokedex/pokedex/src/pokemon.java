2fd024bcac4088426480a344b97cdaa3c9dfe4ca
my-name <alicia.castellanos@students.salle.url.edu>
2025-02-24 11:40:41 +0100

    24/02

---
+++ Pokedex/pokedex/src/pokemon.java
@@ -0,0 +1,14 @@
+public class pokemon {
+    int id;
+    String name;
+    String type;
+    int hitPoints;
+    int defense;
+    int speed;
+    int attack;
+    boolean found;
+
+    public pokemon(int id, String name, String type, int hitPoints, int defense, int speed, boolean found) {
+
+    }
+}
